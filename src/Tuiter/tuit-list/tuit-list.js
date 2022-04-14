863556cd68d710fef8180062b61a860bcdb4be1e
JonMa327-art <71156814+JonMa327-art@users.noreply.github.com>
2022-04-14 14:44:12 -0400

    a9 MONGO SP22

--- src/Tuiter/tuit-list/tuit-list.js
+++ src/Tuiter/tuit-list/tuit-list.js
@@ -1,50 +1,13 @@
-// import React from "react";
-// import TuitListItem from "./tuit-list-item";
-// import { useSelector } from "react-redux";
-// // import './tuits.css';
-
-// const TuitList = () => {
-// const tuitList = useSelector((state) => state.tuitReducer)
-//     return (
-/* <ul className="ttr-tuits list-group">
-    {
-        tuitList.map(tuit =>
-            <TuitListItem tuit={tuit} key={tuit._id} />)
-    }
-</ul> */
-//     );
-// }
-
-// export default TuitList;
-
-
-
 import React from "react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import * as service from "../services/tuit-service.js"
-import TuitListItem from "./tuit-list-item";
 import { updateTuit, createTuit, deleteTuit, findAllTuits } from "../action/tuits-action.js";
 
 const TuitList = () => {
     let tuitList = useSelector((state) => state.tuitReducer)
     // const tuits = useSelector(state => state.tuits);
     const dispatch = useDispatch();
-    const [newTuit, setNewTuit] =
-        useState({ tuit: 'New tuit' });
-
-    // const findAllTuits = async () => {
-    //     const tuits = await service.findAllTuits();
-    //     console.log(tuits)
-    //     tuitList = tuits;
-    //     console.log(tuitList)
-
-    //     dispatch({
-    //         type: 'FIND_ALL_TUITS',
-    //         tuits: tuits
-    //     });
-    // }
-    // useEffect(findAllTuits, []);
+    const [newTuit, setNewTuit] = useState({ tuit: 'New tuit' });
 
     useEffect(() => findAllTuits(dispatch), []);
 
@@ -91,33 +54,3 @@ const TuitList = () => {
 }
 
 export default TuitList;
-
-// import React from "react";
-// import { useEffect, useState } from "react";
-// import * as service from "../../services/tuit-service.js"
-// const TuitList = () => {
-//     //make the start of the list of tuits
-//     const [tuits, setTuits] = useState([]);
-
-//     //because it is async it get called automatically
-//     //Also sets tuits to a list of tuits found by the tuit-services fuction
-//     const findAllTuits = async () => {
-//         const tuits = await service.findAllTuits();
-//         console.log(tuits)
-//         setTuits(tuits)
-//     }
-
-//     useEffect(findAllTuits, []);
-//     // const tuitList = useSelector((state) => state.tuitReducer)
-//     return (
-//         <h1>here {tuits.length}</h1>
-//         // <ul className="ttr-tuits list-group">
-//         //     {
-//         //         tuitList.map(tuit =>
-//         //             <TuitListItem tuit={tuit} key={tuit._id} />)
-//         //     }
-//         // </ul>
-//     );
-// }
-
-// export default TuitList;
